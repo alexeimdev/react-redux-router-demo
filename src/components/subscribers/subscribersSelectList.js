760c118359670670ca30d452eb19e101e255119a
alexeimdev <alexeim.dev@gmail.com>
2021-02-11 20:39:23 +0200

    click handle test

--- src/components/subscribers/subscribersSelectList.js
+++ src/components/subscribers/subscribersSelectList.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 function SubscribersListItem(props) {
 
-    function handleSelectClick() {
-
+    const styles = {
+        item: {
+            cursor: 'pointer',
+            onHover: 'backgrondColor: red'
+        }
     }
 
-    function handleInfoClick() {
+    function handleSelectClick(value) {
+        alert('handleSelectClick ' + value);
+    }
 
+    function handleInfoClick(value) {
+        alert('handleInfoClick', value);
     }
 
     return (
-        <div className="card mt-3 col-12 col-md-8">
+        <div className="card shadow-sm bg-color-red mt-3 col-12 col-md-8"
+            style={styles.item}
+            onClick={() => handleSelectClick(props.item)}>
             <div className="card-body">
                 <h5 className="card-title">{props.item}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <a href="#" className="card-link">Card link</a>
             </div>
         </div>
     )
