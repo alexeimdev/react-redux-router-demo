2320ae5b752874f7b20b4fb82e16e044749d8574
alexeimdev <alexeim.dev@gmail.com>
2021-02-11 18:23:24 +0200

    ui fix

--- src/components/pages/subscriberListUpgradePage.js
+++ src/components/pages/subscriberListUpgradePage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function SubscriberListUpgradePage(props) {
-    
+
     return (
-        <div>
+        <div className="container">
             <h1>איזה מנוי לשדרג?</h1>
-            <div>subscriberList</div>    
+            <div>subscriberList</div>
         </div>
     )
 }
