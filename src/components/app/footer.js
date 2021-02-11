485531ac3f58914e72805fe0a5d4698491134ffd
ALEXEI MELIKOV <ALEXEIME@cellcom.co.il>
2021-02-11 11:40:36 +0200

    added footer

--- src/components/app/footer.js
+++ src/components/app/footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export default function Footer() {
-    
+
     return (
-        <footer className="app-footer">
-            <div>App Footer</div>
+        <footer class="container pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+                <div class="col-6 col-md">
+                    <h5>קישורים</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="link-secondary" href="#">דף הבית</a></li>
+                        <li><a class="link-secondary" href="#">אודות</a></li>
+                        <li><a class="link-secondary" href="#">מכר</a></li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 }
