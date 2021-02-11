1c9316b5fc9b3717d826b1baea40c9b36972a92d
ALEXEI MELIKOV <ALEXEIME@cellcom.co.il>
2021-02-11 11:27:20 +0200

    remove

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,10 +5,10 @@ import Footer from './footer';
 
 export default function App(props) {
     return (
-        <div className="app-wrapper">
+        <>
             <Header />
             <Container />
             <Footer />
-        </div>
+        </>
     )
 }
